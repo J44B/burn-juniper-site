f577e0bb123c64db55fb690cbabc1f2b72637bd9
jochen <jochen@byte23.net>
2025-03-11 14:26:45 +0100

    add styles to header and footer, add instagram and youtube links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,80 @@
+import { Link } from 'react-router';
+import youtube from '../assets/icons/youtube-logo.svg';
+import instagram from '../assets/icons/instagram-logo.svg';
+
 function Footer() {
     return (
-        <div className="h-16 bg-[#547D67] text-white border-b-2">
-            <h1 className="text-3xl">Footer</h1>
-        </div>
+        <footer className="bg-[#547D67] text-white">
+            <div className="h-auto bg-[#547D67] text-white border-b-2">
+                <ul
+                    id="menu-items-container"
+                    className="mt-6 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12"
+                >
+                    <li>
+                        <div
+                            className="text-white transition hover:text-gray-700/75"
+                            href="#"
+                        >
+                            <Link to={'/about'}>About</Link>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div
+                            className="text-white transition hover:text-gray-700/75"
+                            href="#"
+                        >
+                            <Link to={'/live'}>Live</Link>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div
+                            className="text-white transition hover:text-gray-700/75"
+                            href="#"
+                        >
+                            <Link to={'/music'}>Music</Link>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div
+                            className="text-white transition hover:text-gray-700/75"
+                            href="#"
+                        >
+                            <Link to={'/contact'}>Contact</Link>
+                        </div>
+                    </li>
+                </ul>
+
+                <ul
+                    id="social-icons-container"
+                    className="mt-4 mb-6 flex justify-center gap-6 md:gap-8"
+                >
+                    <li>
+                        <Link to={'https://www.instagram.com/burnjuniper/'}>
+                            <img
+                                src={instagram}
+                                alt="Link to Instagram"
+                                width={32}
+                                height={32}
+                            />
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to={'https://www.youtube.com/@burnjuniper'}>
+                            <img
+                                src={youtube}
+                                alt="Link to YouTube"
+                                width={32}
+                                height={32}
+                            />
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </footer>
     );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router';
+
 function Header() {
     return (
-        <div className="h-16 bg-[#547D67] text-white border-b-2">
-            <h1 className="text-3xl">HEADER</h1>
+        <div className="h-12 bg-[#547D67] text-white border-b-2">
+            <div id="bandname" className="flex justify-center items-center">
+                <Link to={'/'} className="text-3xl">
+                    Burn Juniper
+                </Link>
+            </div>
         </div>
     );
 }
